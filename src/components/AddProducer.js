7a6fb63eb6973995f73745818621064b60dac464
Anilkokkul <anilkokkul1997@gmail.com>
2023-12-26 15:22:54 +0530

    updated

--- src/components/AddProducer.js
+++ src/components/AddProducer.js
@@ -117,7 +117,7 @@ function AddProducer({ setModel }) {
                       className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                       type="submit"
                     >
-                      Add Actor
+                      Add Producer
                     </button>
                   </div>
                 </Form>
